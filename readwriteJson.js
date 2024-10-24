3073ba890d16d7435eb46594b53b12fb52ca50f5
chc13 <90738243+chc13@users.noreply.github.com>
2024-10-24 15:27:39 -0700

    readJSONFile checks to see if the file exists now, returns -1 if unavailable

--- readwriteJson.js
+++ readwriteJson.js
@@ -1,6 +1,11 @@
 const fs = require("fs");
 
 function readJSONFile(filePath) {
+  if (!fs.existsSync(filePath)) {
+    console.log("File not found");
+    return -1;
+  }
+
   const data = fs.readFileSync(filePath, "utf8");
   return JSON.parse(data);
 }
